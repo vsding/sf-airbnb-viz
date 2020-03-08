d06810cf4bbc02859ac79bb4db169a1e2a7b317b
Victoria Ding <victoriading19@gmail.com>
2020-03-07 17:39:51 -0800

    add button to show/hide unrated listings

--- q1_map.js
+++ q1_map.js
@@ -8,7 +8,7 @@ let renderMap = () => {
         [-123.2397, 37.424], // Southwest coordinates
         [-120.181, 37.933033] // Northeast coordinates
     ];
-    
+
     var map = new mapboxgl.Map({
         container: 'map',
         style: 'mapbox://styles/mapbox/light-v10',
@@ -16,46 +16,73 @@ let renderMap = () => {
         center: [-122.4594, 37.7349],
         maxBounds: bounds
     });
-    
+
     map.on('load', () => {
         map.addSource('points', {
             type: 'geojson',
             data: geodata
         });
-    
+
         map.addLayer({
             id: 'points',
             type: 'circle',
             source: 'points',
-            paint:
-            {
+            paint: {
                 'circle-radius': 3.5,
-                "circle-color": ["case",
-                    [">=", ["to-number", ["get", "review_scores_rating"]], 90],
-                    "green",
-                    [">=", ["to-number", ["get", "review_scores_rating"]], 70],
-                    "yellow",
-                    ["==", ["to-number", ["get", "review_scores_rating"]], 0],
+                "circle-color": ["case", [">=", ["to-number", ["get", "review_scores_rating"]], 90],
+                    "green", [">=", ["to-number", ["get", "review_scores_rating"]], 70],
+                    "yellow", ["==", ["to-number", ["get", "review_scores_rating"]], 0],
                     "gray", "red"
                 ],
                 'circle-opacity': 0.7,
             }
         });
-    
+
         function filterByRating(num, map) {
             var filter = ['<=', ["to-number", ["get", "review_scores_rating"]], num];
             map.setFilter('points', filter);
             document.getElementById('rating_range').textContent = `Rating <= ${num}`;
         }
-    
+
         filterByRating(50, map);
-        document.getElementById('slider').addEventListener('input', function (e) {
-            let num = parseInt(e.target.value, 10);
-            filterByRating(num, map);
+        hideUnrated(50);
+
+        var shouldHideUnrated = true;
+
+        function hideUnrated(ratingThreshold) {
+            var filter = ["all", ['>', ["to-number", ["get", "review_scores_rating"]], 0],
+                ['<=', ["to-number", ["get", "review_scores_rating"]], ratingThreshold]
+            ];
+            map.setFilter('points', filter);
+        }
+
+        var button = document.getElementById('toggleListingsButton');
+        button.addEventListener('click', updateButton);
+
+        function updateButton() {
+            shouldHideUnrated = !shouldHideUnrated;
+            if (button.value === 'Hide unrated listings') {
+                button.value = 'Show unrated listings';
+            } else {
+                button.value = 'Hide unrated listings';
+            }
+            if (shouldHideUnrated) {
+                hideUnrated(Number(document.getElementById('slider').value));
+            } else { // filter by rating only
+                filterByRating(Number(document.getElementById('slider').value), map);
+            }
+        }
+
+        document.getElementById('slider').addEventListener('input', function(e) {
+            let ratingThreshold = parseInt(e.target.value, 10);
+            filterByRating(ratingThreshold, map);
+            if (shouldHideUnrated) {
+                hideUnrated(ratingThreshold);
+            }
         });
     });
-    
-    map.on('click', 'points', function (e) {
+
+    map.on('click', 'points', function(e) {
         let coordinates = e.features[0].geometry.coordinates.slice();
         let name = e.features[0].properties.name;
         let price = e.features[0].properties.price;
@@ -64,24 +91,24 @@ let renderMap = () => {
         let num_beds = Math.trunc(e.features[0].properties.beds);
         let num_accommodate = Math.trunc(e.features[0].properties.accommodates);
         let rating = e.features[0].properties.review_scores_rating;
-    
+
         while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
             coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
         }
-    
-        new mapboxgl.Popup()
+
+        new mapboxgl.Popup({ className: 'mapPopup' })
             .setLngLat(coordinates)
             .setHTML('<center><h3>' + name + '</h4><p>' + price + ' per night</p><p>' + num_bedrooms + ' bed, ' + num_bathrooms + ' bath, accommodates ' + num_accommodate + '</p><p>' + 'Rating: ' + rating + '</p></center>')
             .addTo(map);
     });
-    
-    map.on('mouseenter', 'points', function () {
+
+    map.on('mouseenter', 'points', function() {
         map.getCanvas().style.cursor = 'pointer';
     });
-    
-    map.on('mouseleave', 'points', function () {
+
+    map.on('mouseleave', 'points', function() {
         map.getCanvas().style.cursor = '';
     });
 }
 
-export {renderMap};
+export { renderMap };
